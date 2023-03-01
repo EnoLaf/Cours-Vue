838fd8cce99ebb0202fdd03798732b00ef4fa007
Enora Lafforgue <100050819+EnoLaf@users.noreply.github.com>
2023-03-01 16:57:07 +0100

    Add files via upload

---
+++ component communication (props)/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>titre</h1>
+    <ul>
+        <un-ami 
+            leNom="Aragsane" 
+            lePhone="0102030405" 
+            leMail="aragsane@hn.com">
+        </un-ami>
+        <un-ami 
+            leNom="Jonathan" 
+            lePhone="0102030405" 
+            leMail="jonathan@simon.com">
+        </un-ami>
+    </ul>
+</template>
+
+<script></script>
---
+++ component communication (props)/src/components/UnAmi.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card border-dark mb-3">
+        <div class="card-header">{{leNom}}</div>
+        <div class="card-body text-dark">
+            <button @click="toggleDetails()" type="button" class="btn btn-dark mb-1">{{detailsVisibles ? 'Masquer':'Voir'}}</button>
+            <ul v-if="detailsVisibles" class="list-group list-group-flush">
+                <li class="list-group-item">{{leMail}}</li>
+                <li class="list-group-item">{{lePhone}}</li>                          
+            </ul>
+         </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:[
+            "leNom",
+            "lePhone",
+            "leMail",
+        ],
+        data(){
+            return{
+                detailsVisibles:false,
+            }
+        },
+        methods: {
+            toggleDetails(){
+                this.detailsVisibles =! this.detailsVisibles;
+            }
+        },
+    }
+</script>
